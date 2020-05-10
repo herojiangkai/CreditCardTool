6669841a64686ce8c162704ca250f399aeb24318
jiangkai <herojiangkai@163.com>
2020-05-10 22:29:41 +0900

    履歴からの自動補完機能追加

--- sumOld/searchDetails.php
+++ sumOld/searchDetails.php
@@ -1,3 +1,148 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+
+
+    <link rel="stylesheet" href="/OnsenUI/OnsenUI-dist-2.10.10/css/onsenui.css">
+    <link rel="stylesheet" href="/OnsenUI/OnsenUI-dist-2.10.10/css/onsen-css-components.min.css">
+    <script src="/OnsenUI/OnsenUI-dist-2.10.10/js/onsenui.min.js"></script>
+
+    <link rel="stylesheet" href="/jquery/jquery-ui-1.12.1.custom/jquery-ui.css" />
+    <script src="/jquery/jquery-ui-1.12.1.custom/external/jquery/jquery.js"></script>
+    <script src="/jquery/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+
+
+    <script>
+        window.fn = {};
+
+        window.fn.open = function () {
+            var menu = document.getElementById('menu');
+            menu.open();
+        };
+
+        window.fn.load = function (page) {
+            var content = document.getElementById('content');
+            var menu = document.getElementById('menu');
+            content.load(page)
+                .then(menu.close.bind(menu));
+        };
+        window.fn.close = function () {
+            var menu = document.getElementById('menu');
+            menu.close();
+        };
+    </script>
+
+    <script>
+        function showStoreNames() {
+            $("#storeName").autocomplete({
+                minLength: 0,
+                source: "/forAjax/getStoreNames.php",
+                focus: function () {
+                    return false;
+                },
+                select: function (event, ui) {
+                    $this = $(this);
+                    setTimeout(function () {
+                        $this.blur();
+                    }, 1);
+                },
+                appendTo: ".content"
+            }).focus(function () {
+                $(this).autocomplete("search");//姜メモ:ここは「search」固定
+                return false;
+            }
+            );
+        };
+        function showPaymentMethods() {
+            $("#paymentMethod").autocomplete({
+                minLength: 0,
+                source: "/forAjax/getPaymentMethods.php",
+                focus: function () {
+                    return false;
+                },
+                select: function (event, ui) {
+                    $this = $(this);
+                    setTimeout(function () {
+                        $this.blur();
+                    }, 1);
+                },
+                appendTo: ".content"
+            }).focus(function () {
+                $(this).autocomplete("search");//姜メモ:ここは「search」固定
+                return false;
+            }
+            );
+        };
+        function showComments() {
+            $("#comment").autocomplete({
+                minLength: 0,
+                source: "/forAjax/getComments.php",
+                focus: function () {
+                    return false;
+                },
+                select: function (event, ui) {
+                    $this = $(this);
+                    setTimeout(function () {
+                        $this.blur();
+                    }, 1);
+                },
+                appendTo: ".content"
+            }).focus(function () {
+                $(this).autocomplete("search");//姜メモ:ここは「search」固定
+                return false;
+            }
+            );
+        };
+
+        
+
+    </script>
+
+
+</head>
+
+<body>
+    <ons-splitter>
+        <ons-splitter-side id="menu" side="right" width="220px" collapse swipeable>
+            <ons-page>
+                <ons-list>
+                    <ons-list-item onclick="location='/'" tappable>
+                        出費登録
+                    </ons-list-item>
+                    <ons-list-item onclick="location='/sum'" tappable>
+                        直近月間集計
+                    </ons-list-item>
+                    <ons-list-item onclick="location='/q'" tappable>
+                        入力照会・変更
+                    </ons-list-item>
+                    <ons-list-item onclick="location='/sumOld'" tappable>
+                        過去集計
+                    </ons-list-item>
+                </ons-list>
+            </ons-page>
+        </ons-splitter-side>
+        <ons-splitter-content id="content">
+
+
+
+        <ons-page>
+    <ons-toolbar>
+        <div class="right">
+            <ons-toolbar-button onclick="fn.open()">
+                <ons-icon icon="md-menu"></ons-icon>
+            </ons-toolbar-button>
+        </div>
+        <div class="center">
+        明細検索
+        </div>
+    </ons-toolbar>
+
+    <div class="content">
+
+
+
 <html>
 <head>
     <title>明細検索</title>
@@ -24,20 +169,20 @@
 </head>
 
 <body>
-<h3 align="center">明細検索</h3>
+<!-- <h3 align="center">明細検索</h3> -->
 <div align="right">
     <a href="/">入力画面</a>
     <a href="./">戻る</a>
 </div>
 
 <div align="center">
-<form  action="showMonthDetails.php" method="get" autocomplete="on">
+<form  action="showMonthDetails.php" method="get" autocomplete="off">
 <input type="hidden" name="action" value="search">
 
 <table>
 <tr><td>期間 from:</td><td><input type="date" name="startDate"></td></tr>
 <tr><td>期間 to:</td><td><input type="date" name="endDate"></td></tr>
-<tr><td>利用店名:</td><td><input type="text" name="store"></td></tr>
+<tr><td>利用店名:</td><td><input type="text" name="store" id="storeName" onfocus="showStoreNames()"></td></tr>
 <tr><td>金額 min:</td><td><input type="number" name="startAmount"></td></tr>
 <tr><td>金額 max:</td><td><input type="number" name="endAmount"></td></tr>
 <tr><td>利用区分:</td><td><a href="javascript:checkAll()">全選択</a> <a href="javascript:unCheckAll()">全解除</a></td></tr>
@@ -53,13 +198,35 @@
 <tr><td></td><td><input type="checkbox" name="users[]" value="lumine(7914)" checked>Lumine(7914)</td></tr>
 <tr><td></td><td><input type="checkbox" name="users[]" value="ft(8518)" checked>ファミマT(8518)</td></tr>
 <tr><td></td><td><input type="checkbox" name="users[]" value="toho(8000)" checked>TOHO(8000)</td></tr>
-<tr><td>支払方法:</td><td><input type="text" name="paymentMethod"></td></tr>
-<tr><td>備考:</td><td><input type="text" name="comment"></td></tr>
+<tr><td>支払方法:</td><td><input type="text" name="paymentMethod" id="paymentMethod" onfocus="showPaymentMethods()"></td></tr>
+<tr><td>備考:</td><td><input type="text" name="comment" id="comment" onfocus="showComments()"></td></tr>
 <tr><td></td><td><input type="submit" value="検索"></td></tr>
 </table>
 
 </form>
 </div>
 <br><br><br><br><br><br>
+
+<script>
+        showStoreNames();
+        showPaymentMethods();
+        showComments();
+
+</script>
+
 </body>
+</html>
+
+
+
+
+
+</div>
+</ons-page>
+
+</ons-splitter-content>
+    </ons-splitter>
+
+</body>
+
 </html>
